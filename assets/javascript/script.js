7019ba799364792fe85efc069d6a47a21607db00
forxtoisate <hwoolford@gmail.com>
2023-10-31 16:16:16 -0500

    added URL for geocode api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,8 @@
 var apiKey = "1cf183538d8a0988ab3207c9e8585519";
 var city;
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
+
 
 // Now that you have created your query URL, you only need to call the Fetch API to pass the query URL in as a parameter, as shown in the following example:
 // fetch(queryURL)
