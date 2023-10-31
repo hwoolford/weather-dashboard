a0af0c3964c1a483aeadc298a8cc5ef64b1f83e9
forxtoisate <hwoolford@gmail.com>
2023-10-31 14:45:07 -0500

    updated js with api key and the query URL for open weather

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+var apiKey = "1cf183538d8a0988ab3207c9e8585519";
+var city;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+// Now that you have created your query URL, you only need to call the Fetch API to pass the query URL in as a parameter, as shown in the following example:
+// fetch(queryURL)
+// Remember that the query URL won't work automatically as it's written. You'll need to adjust your application to accept user input, to store in the city variable that you've created.
