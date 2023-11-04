3edae7202413f96d63726a35d747db1be017a164
forxtoisate <hwoolford@gmail.com>
2023-11-04 12:50:07 -0500

    Added function to extract name, lat, and lon from Geocoding API

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,11 @@
 const apiKey = "1cf183538d8a0988ab3207c9e8585519";
-var city;
+let city;
 const userCity = document.getElementById("search-box");
 const searchBtn = document.getElementById("search-button");
-const fetchURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+// const fetchURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 const fetchURLCity = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 const geocodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
-const fetchCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+// const fetchCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
 
 // add event listener for searchBtn
@@ -13,11 +13,29 @@ const fetchCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lati
 // Get lat and lon from geocodingAPI
 // I will have to get the searched city from local storage and append it to the <ul> class "history" and will need to create new <li> items for each city searched. This will be done with a for loop
 // I will need to set an event listener for clicking on the <li> which will trigger city weather
-// http://api.openweathermap.org/geo/1.0/direct?q=Houston&appid=1cf183538d8a0988ab3207c9e8585519
+const weather = 'http://api.openweathermap.org/geo/1.0/direct?q=Houston&appid=1cf183538d8a0988ab3207c9e8585519'
 
 // http://api.openweathermap.org/data/2.5/forecast?lat=29.7589382&lon=-95.3676974&appid=1cf183538d8a0988ab3207c9e8585519&units=imperial
 
 // http://api.openweathermap.org/data/2.5/weather?lat=29.7589382&lon=-95.3676974&appid=1cf183538d8a0988ab3207c9e8585519&units=imperial
 
+function getCoordinates() {
+    fetch(weather)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        for (let i = 0; i < data.length; i++) {
+            nameCity = data[i].name
+            latitude = data[i].lat
+            longitude = data[i].lon
+            console.log(nameCity)
+            console.log(latitude)
+            console.log(longitude)
+        }
+    })
+}
+getCoordinates();
 
-
+   
+// searchBtn.addEventListener("click", getCoordinates())
