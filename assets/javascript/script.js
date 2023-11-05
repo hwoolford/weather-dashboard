c9b5ca00b99184a986ebc1a435e64fe15c1c2dc4
forxtoisate <hwoolford@gmail.com>
2023-11-05 12:34:38 -0600

    Wrote code to Fetch 5-day weather and am working on code to append it to the page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,17 +3,14 @@ const userInput = userCity.value.trim();
 const searchBtn = document.getElementById("search-button");
 const searchHistory = document.getElementsByClassName("search-history");
 const tableBody = document.getElementById("history");
+const nameCity = document.getElementsByClassName("city-name");
+const currentInfo = document.getElementsByClassName("current-info")
+const currentWeather = document.getElementsByClassName("current-weather")
 const apiKey = "1cf183538d8a0988ab3207c9e8585519";
+let city;
+let latitude;
+let longitude;
 
-// const fetchURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-
-// const fetchCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-
-// add event listener for searchBtn
-// search btn will trigger city weather and will set search term in local storage
-// Get lat and lon from geocodingAPI
-// I will have to get the searched city from local storage and append it to the <ul> class "history" and will need to create new <li> items for each city searched. This will be done with a for loop
-// I will need to set an event listener for clicking on the <li> which will trigger city weather
 
 $(document).ready(function () {
   $("#search-button").on("click", function () {
@@ -21,21 +18,19 @@ $(document).ready(function () {
     localStorage.setItem("history", JSON.stringify(userInput));
 
     function getCoordinates() {
-      let city = userInput
+      let city = userInput;
       const geocodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
+
       fetch(geocodeURL)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
           for (let i = 0; i < data.length; i++) {
-            cityName = data[i].name;
-            latitude = data[i].lat;
-            longitude = data[i].lon;
-            console.log(city);
-            console.log(latitude);
-            console.log(longitude);
-            
+            let city = data[i].name;
+            let latitude = data[i].lat;
+            let longitude = data[i].lon;
+                        
             let storedCity = JSON.parse(localStorage.getItem("history"));
             let createTableRow = document.createElement("tr");
             let tableData = document.createElement("td");
@@ -45,10 +40,65 @@ $(document).ready(function () {
             tableData.appendChild(info);
             createTableRow.appendChild(tableData);
             tableBody.appendChild(createTableRow);
+
+            function getForecast(latitude, longitude, city) {
+              console.log(city);
+              console.log(latitude);
+              console.log(longitude);
+
+              const fetchForecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+
+              fetch(fetchForecastURL)
+              .then(function(response) {
+                return response.json();
+              })
+              .then(function(data) {
+                console.log(data)
+                for (let i = 0; i < data.list.length; i++) {
+
+                
+                  let cityName = data.city.name
+                  console.log(cityName)
+                  let temp = data.list[i].main.temp
+                  console.log(temp)
+                  let humidity = data.list[i].main.humidity
+                  console.log(humidity)
+                  let wind = data.list[i].wind.speed
+                  console.log(wind)
+                  let currentIcon = data.list[i].weather.Icon
+                  console.log(currentIcon)
+                  let date = data.list[i].dt_txt
+                  console.log(date)
+
+                  let divElement = document.createElement("div")
+                  
+                  nameCity.textContent = cityName + " (" + date + ")"
+                  console.log(nameCity)
+                  currentInfo.innerHTML = nameCity
+                  // divElement.append(currentInfo)
+                }
+              })
+              .catch(function(error) {
+                console.log(error);
+              })
+            }
+            
+            
+            getForecast(latitude, longitude, city);
           }
         });
     }
 
     getCoordinates();
+   
+
+    
+
+    
+      
+      
+
+      
+
   });
 });
