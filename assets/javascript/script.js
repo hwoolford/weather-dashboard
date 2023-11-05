83681234b4884cd8a773cf6c24f4559dcea5d03c
forxtoisate <hwoolford@gmail.com>
2023-11-05 14:02:28 -0600

    Updated code to extract the current weather conditions and put it on the dashboard

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,13 +4,15 @@ const searchBtn = document.getElementById("search-button");
 const searchHistory = document.getElementsByClassName("search-history");
 const tableBody = document.getElementById("history");
 const nameCity = document.getElementsByClassName("city-name");
-const currentInfo = document.getElementsByClassName("current-info")
-const currentWeather = document.getElementsByClassName("current-weather")
+const currentInfo = document.getElementsByClassName("current-info");
+const currentTemp = document.getElementById("current-temp");
+const currentWind = document.getElementById("current-wind");
+const currentHumidity = document.getElementById("current-humidity");
+
 const apiKey = "1cf183538d8a0988ab3207c9e8585519";
 let city;
 let latitude;
 let longitude;
-
 
 $(document).ready(function () {
   $("#search-button").on("click", function () {
@@ -30,7 +32,7 @@ $(document).ready(function () {
             let city = data[i].name;
             let latitude = data[i].lat;
             let longitude = data[i].lon;
-                        
+
             let storedCity = JSON.parse(localStorage.getItem("history"));
             let createTableRow = document.createElement("tr");
             let tableData = document.createElement("td");
@@ -49,54 +51,56 @@ $(document).ready(function () {
               const fetchForecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
               fetch(fetchForecastURL)
-              .then(function(response) {
-                return response.json();
-              })
-              .then(function(data) {
-                console.log(data)
-                for (let i = 0; i < data.list.length; i++) {
+                .then(function (response) {
+                  return response.json();
+                })
+                .then(function (data) {
+                  console.log(data);
+                  for (let i = 0; i < data.list.length; i++) {
+                    let cityName = data.city.name;
+                    // console.log(cityName)
+                    let temp = data.list[i].main.temp;
+                    // console.log(temp)
+                    let humidity = data.list[i].main.humidity;
+                    // console.log(humidity)
+                    let wind = data.list[i].wind.speed;
+                    // console.log(wind)
+                    let currentIcon = data.list[i].weather.Icon;
+                    // console.log(currentIcon)
+                    let date = data.list[i].dt_txt;
+                    // console.log(date);
+                    
+                    // Getting current results for the current weather data
+                    let currentDate = data.list[0].dt_txt;
+                    let currentTemperature = data.list[0].main.temp;
+                    let currentWindSpeed = data.list[0].wind.speed;
+                    let currentHumid = data.list[0].main.humidity;
 
-                
-                  let cityName = data.city.name
-                  console.log(cityName)
-                  let temp = data.list[i].main.temp
-                  console.log(temp)
-                  let humidity = data.list[i].main.humidity
-                  console.log(humidity)
-                  let wind = data.list[i].wind.speed
-                  console.log(wind)
-                  let currentIcon = data.list[i].weather.Icon
-                  console.log(currentIcon)
-                  let date = data.list[i].dt_txt
-                  console.log(date)
+                    nameCity[0].textContent =
+                      cityName + " (" + currentDate.split(" ")[0] + ")";
+                    console.log(nameCity[0].textContent);
+                    currentInfo.textContent = nameCity[0].textContent;
 
-                            
-                  nameCity[0].textContent = cityName + " (" + date.split(" ")[0] + ")"
-                  console.log(nameCity[0].textContent)
-                  currentInfo.textContent = nameCity[0].textContent
-                }
-              })
-              .catch(function(error) {
-                console.log(error);
-              })
+                    currentTemp.textContent = "Temperature: " + currentTemperature + "°F";
+                    console.log(currentTemp.textContent)
+                    currentInfo.textContent = currentTemp.textContent;
+
+                    currentWind.textContent = "Wind: " + currentWindSpeed + " MPH"
+                    currentInfo.textContent = currentWind.textContent
+
+                    currentHumidity.textContent = "Humidity: " + currentHumid + "%"
+                  }
+                })
+                .catch(function (error) {
+                  console.log(error);
+                });
             }
-            
-            
+
             getForecast(latitude, longitude, city);
           }
         });
     }
 
     getCoordinates();
-   
-
-    
-
-    
-      
-      
-
-      
-
   });
 });
